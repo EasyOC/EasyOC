77632faf621155324df6e391a7b15c6b74b7ff07
Zhu yinlong <Xianglong.Gao@ext.saint-gobain.com>
2021-12-24 15:10:40 +0800

    add User API

---
+++ src/Modules/EasyOC.WebApi/Dto/PagedResult.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyOC.WebApi.Dto
+{
+    public class PagedResultDto<T>
+    {
+        public PagedResultDto(int total, IEnumerable<T> items)
+        {
+            Total = total;
+            Items = items;
+        }
+        public int Total { get; set; }
+        public IEnumerable<T> Items { get; set; }
+    }
+    //
+    // 摘要:
+    //     Implements Abp.Application.Services.Dto.IListResult`1.
+    //
+    // 类型参数:
+    //   T:
+    //     Type of the items in the Abp.Application.Services.Dto.ListResultDto`1.Items list
+    public class ListResultDto<T> : IListResult<T>
+    {
+        //
+        // 摘要:
+        //     Creates a new Abp.Application.Services.Dto.ListResultDto`1 object.
+        public ListResultDto() {
+           
+        }
+        //
+        // 摘要:
+        //     Creates a new Abp.Application.Services.Dto.ListResultDto`1 object.
+        //
+        // 参数:
+        //   items:
+        //     List of items
+        public ListResultDto(IReadOnlyList<T> items)
+        {
+            Items = items;
+        }
+
+        //
+        // 摘要:
+        //     List of items.
+        public IReadOnlyList<T> Items { get; set; }
+    }
+
+    //
+    // 摘要:
+    //     This interface is defined to standardize to return a list of items to clients.
+    //
+    // 类型参数:
+    //   T:
+    //     Type of the items in the Abp.Application.Services.Dto.IListResult`1.Items list
+    public interface IListResult<T>
+    {
+        //
+        // 摘要:
+        //     List of items.
+        IReadOnlyList<T> Items { get; set; }
+    }
+}
---
+++ src/Modules/EasyOC.WebApi/Services/IUserAppService.cs
@@ -0,0 +1,27 @@
+﻿using EasyOC.WebApi.Dto;
+using OrchardCore.DisplayManagement;
+using OrchardCore.Navigation;
+using OrchardCore.Users.Models;
+using OrchardCore.Users.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyOC.WebApi.Services
+{
+     public interface IUserAppService
+    {
+        Task<PagedResultDto<User>> Index(UserIndexOptions options, PagerParameters pagerParameters);
+        Task BulkAction(UserIndexOptions options, IEnumerable<string> itemIds);
+        Task<IShape> Create();
+        Task CreatePost(User user);
+        Task<User> GetUserAsync(string id);
+        Task EditPost(string id, string returnUrl);
+        Task Delete(string id);
+        Task EditPassword(string id);
+        Task EditPassword(UserDto model);
+
+    }
+}
