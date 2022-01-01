2a26b101549a6f16dc84e85b956b20405788d912
Tony Han <yanzhong.han@jizhousoft.com>
2022-01-01 18:07:39 +0800

    移除继承

--- src/Modules/EasyOC.OrchardCore.ContentExtentions/AppServices/Dtos/ContentTypeListItemDto.cs
+++ src/Modules/EasyOC.OrchardCore.ContentExtentions/AppServices/Dtos/ContentTypeListItemDto.cs
@@ -1,16 +1,14 @@
 ﻿using AutoMapper;
-using AutoMapper.Configuration.Annotations;
-using EasyOC.Core;
-using Newtonsoft.Json.Linq;
 using OrchardCore.ContentManagement.Metadata.Models;
 
 namespace EasyOC.OrchardCore.ContentExtentions.AppServices.Dtos
 {
     [AutoMap(typeof(ContentTypeDefinition))]
-    public class ContentTypeListItemDto : ContentDefinitionDto
+    public class ContentTypeListItemDto 
     {
         public string DisplayName { get; set; }
         public string Stereotype { get; internal set; }
+        public string Name { get; set; }
 
     }
 }
