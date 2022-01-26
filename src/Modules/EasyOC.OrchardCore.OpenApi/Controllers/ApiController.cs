fc73eaa0257e2ef48397367b1bd40984ec490560
Tony Han <huarui2219@qq.com>
2022-01-26 14:06:26 +0800

    Revert "导出逻辑需求定义"
    This reverts commit 91092fc33d5b6dd74286db788caeb2536fb910e6.

--- src/Modules/EasyOC.OrchardCore.OpenApi/Controllers/ApiController.cs
+++ src/Modules/EasyOC.OrchardCore.OpenApi/Controllers/ApiController.cs
@@ -1,5 +1,4 @@
-﻿using EasyOC.OrchardCore.OpenApi.Services;
-using Microsoft.AspNetCore.Authorization;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using OrchardCore.Lucene;
 using OrchardCore.Queries;
@@ -33,13 +32,7 @@ namespace EasyOC.OrchardCore.OpenApi.Controllers
             {
                 returnDocuments = sqlQuery.ReturnDocuments;
             }
-            ///TODO: 执行导出逻辑
-            ///TODO: 1. 获取类型名称，可以修改方法签名，但参数超过3个 需要封装成对象
-            ///TODO: 2. 根据指定类型生成Excel Mapping结构
-            ///TODO: 3. 从类型定义遍历属性取值，也就是把多维结构转换为平面结构 参考 TS 代码：ContentHelper.getAllFields, 可以把类似的类型解析逻辑迁移到  本项目的 Service 文件夹下 
-            ///         也可参考： 但此处是处理用户扩展信息，内容类型比用户信息少一层 <seealso cref="UsersAppService.FillAdditionalData"/>
-            ///TODO: 4  先构建处平面定义，包含一系列属性路径，属性名称 等，需要考虑不同Part下存在相同显示名称的字段
-            ///TODO: 5  将内容写入到Excel    <seealso cref="UsersAppService.FillAdditionalData"/> 的 241行  有直接访问 JsonToken 的方法，支持Linq
+            //
 
             return null;
         }
