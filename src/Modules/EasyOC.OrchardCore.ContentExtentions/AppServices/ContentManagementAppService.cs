816843cbe607204ec514ba05c08dfd04d6242fa6
Tony Han <huarui2219@qq.com>
2022-02-15 14:36:59 +0800

    remove ContentManagerExtentions

--- src/Modules/EasyOC.OrchardCore.ContentExtentions/AppServices/ContentManagementAppService.cs
+++ src/Modules/EasyOC.OrchardCore.ContentExtentions/AppServices/ContentManagementAppService.cs
@@ -3,6 +3,7 @@ using EasyOC.OrchardCore.ContentExtentions.AppServices.Dtos;
 using EasyOC.OrchardCore.ContentExtentions.Models;
 using Microsoft.Extensions.DependencyInjection;
 using OrchardCore.ContentManagement.Metadata;
+using OrchardCore.ContentManagement.Metadata.Models;
 using OrchardCore.ContentManagement.Metadata.Settings;
 using OrchardCore.Lucene;
 using OrchardCore.Queries;
@@ -70,20 +71,89 @@ namespace EasyOC.OrchardCore.ContentExtentions.AppServices
             var typeDefinition = _contentDefinitionManager.LoadTypeDefinition(name);
             return typeDefinition.ToDto(withSettings);
         }
+
+
+        [EOCAuthorization(OCPermissions.EditContentTypes)]
+        public async Task<IEnumerable<QueryDefDto>> ListLuceneQueriesAsync()
+        {
+            var queries = (await _queryManager.ListQueriesAsync()).OfType<LuceneQuery>();
+            return ObjectMapper.Map<IEnumerable<QueryDefDto>>(queries);
+        }
         /// <summary>
         /// 获取指定类型的字段清单
         /// </summary>
         /// <param name="typeName"></param>
         /// <returns></returns>
-        public List<ContentFieldsMappingDto> GetFields(string typeName) => _contentDefinitionManager.GetAllFields(typeName).ToList();
+        public List<ContentFieldsMappingDto> GetFields(string typeName)
+        {
+            var typeDef = _contentDefinitionManager.GetTypeDefinition(typeName);
+            var fields = new List<ContentFieldsMappingDto>();
+            fields.Add(new ContentFieldsMappingDto { DisplayName = S["ID"].Value, FieldName = "ContentItemId", IsContentItemProperty = true });
+            fields.Add(new ContentFieldsMappingDto { DisplayName = S["版本号"].Value, FieldName = "ContentItemVersionId", IsContentItemProperty = true });
+            fields.Add(new ContentFieldsMappingDto { DisplayName = S["内容类型"].Value, FieldName = "ContentType", IsContentItemProperty = true });
+            fields.Add(new ContentFieldsMappingDto { DisplayName = S["标题"].Value, FieldName = "DisplayText", IsContentItemProperty = true });
+            fields.Add(new ContentFieldsMappingDto { DisplayName = S["最新版"].Value, FieldName = "Latest", IsContentItemProperty = true });
+            fields.Add(new ContentFieldsMappingDto { DisplayName = S["已发布"].Value, FieldName = "Published", IsContentItemProperty = true });
+            fields.Add(new ContentFieldsMappingDto { DisplayName = S["修改时间"].Value, FieldName = "ModifiedUtc", IsContentItemProperty = true });
+            fields.Add(new ContentFieldsMappingDto { DisplayName = S["发布时间"].Value, FieldName = "PublishedUtc", IsContentItemProperty = true });
+            fields.Add(new ContentFieldsMappingDto { DisplayName = S["发布时间"].Value, FieldName = "CreatedUtc", IsContentItemProperty = true });
+            fields.Add(new ContentFieldsMappingDto { DisplayName = S["归属人"].Value, FieldName = "Owner", IsContentItemProperty = true });
+            fields.Add(new ContentFieldsMappingDto { DisplayName = S["作者"].Value, FieldName = "Author", IsContentItemProperty = true });
+            foreach (var item in fields)
+            {
+                item.LastValueKey = item.KeyPath = item.FieldName;
+            }
+            foreach (var part in typeDef.Parts)
+            {
+                foreach (var field in part.PartDefinition.Fields)
+                {
+                    var lastKey = GetFiledValuePath(field.FieldDefinition.Name);
+                    fields.Add(new ContentFieldsMappingDto
+                    {
+                        DisplayName = S[field.DisplayName()].Value,
+                        Description = field.Description(),
+                        FieldName = field.Name,
+                        PartName = part.Name,
+                        PartDisplayName = part.DisplayName(),
+                        KeyPath = $"{part.Name}.{field.Name}.{lastKey}",
+                        LastValueKey = lastKey,
+                        FieldSettings = field.Settings,
+                        FieldType = field.FieldDefinition.Name
+                    });
+                }
 
-        [EOCAuthorization(OCPermissions.EditContentTypes)]
-        public async Task<IEnumerable<QueryDefDto>> ListLuceneQueriesAsync()
-        { 
-            var queries = (await _queryManager.ListQueriesAsync()).OfType<LuceneQuery>();
-            return ObjectMapper.Map<IEnumerable<QueryDefDto>>(queries);
+            }
+
+
+            return fields;
         }
 
+        public static string GetFiledValuePath(string fieldName)
+        {
+            string valuePath;
+            switch (fieldName)
+            {
+                case "TextField":
+                    valuePath = "Text";
+                    break;
+                case "BooleanField":
+                case "DateField":
+                case "TimeField":
+                case "DateTimefield":
+                case "NumericField":
+                    valuePath = "Value";
+                    break;
+                case "ContentPickerField":
+                    valuePath = "ContentItemIds[0]";
+                    break;
+                case "UserPickerField":
+                    valuePath = "UserIds[0]";
+                    break;
+                default:
+                    return null;
+            }
+            return valuePath;
+        }
     }
 }
 
