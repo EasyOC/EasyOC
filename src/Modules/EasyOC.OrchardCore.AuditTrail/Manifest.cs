eee7229959a02036b49dcdebe258dd2361611025
Tony Han <huarui2219@qq.com>
2022-05-31 00:46:16 +0800

    Audit Trail

---
+++ src/Modules/EasyOC.OrchardCore.AuditTrail/Manifest.cs
@@ -0,0 +1,10 @@
+using OrchardCore.Modules.Manifest;
+
+[assembly: Module(
+    Name = "EasyOC.OrchardCore.AuditTrail",
+    Author = "The Orchard Core Team",
+    Website = "https://orchardcore.net",
+    Version = "0.0.1",
+    Description = "EasyOC.OrchardCore.AuditTrail",
+    Category = "Content Management"
+)]
