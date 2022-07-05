40c94a95e47437637f077ac6f15b9376b7e33e6c
zyt <zhangyatao@jizhousoft.com>
2022-07-05 20:59:48 +0800

    修改错误

--- src/Modules/EasyOC.Lucene/Startup.cs
+++ src/Modules/EasyOC.Lucene/Startup.cs
@@ -1,9 +1,4 @@
-using System;
-using Fluid;
-using Lucene.Net.Analysis.Cn;
 using Lucene.Net.Analysis.Core;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.DependencyInjection;
 using OrchardCore.Lucene;
 using OrchardCore.Lucene.Services;
@@ -20,19 +15,6 @@ namespace EasyOC.Lucene
             {
                 o.Analyzers.Add(new LuceneAnalyzer("KeywordAnalyzer", new KeywordAnalyzer()));
             });
-        }
-
-        public override void Configure(IApplicationBuilder builder, IEndpointRouteBuilder routes, IServiceProvider serviceProvider)
-        {
-            routes.MapAreaControllerRoute(
-                name: "Home",
-                areaName: "EasyOC.Lucene",
-                pattern: "Home/Index",
-                defaults: new
-                {
-                    controller = "Home", action = "Index"
-                }
-            );
         }
     }
 }
