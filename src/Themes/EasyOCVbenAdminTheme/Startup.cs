1c8c6e8e553149e6a5b9e144eb19b6a694ab776e
Tony Han <huarui2219@qq.com>
2022-08-05 01:19:08 +0800

    vbenTheme

--- src/Themes/EasyOCVbenAdminTheme/Startup.cs
+++ src/Themes/EasyOCVbenAdminTheme/Startup.cs
@@ -1,7 +1,14 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
 using OrchardCore.Modules;
 using OrchardCore.ResourceManagement;
+using OrchardCore.Users;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace EasyOCVbenAdminTheme
 {
@@ -10,6 +17,20 @@ namespace EasyOCVbenAdminTheme
         public override void ConfigureServices(IServiceCollection serviceCollection)
         {
             serviceCollection.AddTransient<IConfigureOptions<ResourceManagementOptions>, ResourceManagementOptionsConfiguration>();
+        }
+        public override void Configure(IApplicationBuilder app, IEndpointRouteBuilder routes, IServiceProvider serviceProvider)
+        {
+            var userOptions = serviceProvider.GetRequiredService<IOptions<UserOptions>>().Value;
+
+            // app.UseEndpoints(endpoints =>
+            // {
+            //     endpoints.MapGet(userOptions.LoginPath, new RequestDelegate(ctx =>
+            //         {
+            //             ctx.Response.Redirect("EasyOCVbenAdminTheme/Vben/index.html");
+            //             return Task.CompletedTask;
+            //         }));
+            // });
+
         }
     }
 }
