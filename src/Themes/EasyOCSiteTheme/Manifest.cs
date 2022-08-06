81b3094c1278de81f31ecb5a27902758a63617c3
Tony Han <huarui2219@qq.com>
2022-08-06 13:59:23 +0800

    Fix Site Theme

--- src/Themes/EasyOCSiteTheme/Manifest.cs
+++ src/Themes/EasyOCSiteTheme/Manifest.cs
@@ -1,15 +1,14 @@
-﻿using OrchardCore.DisplayManagement.Manifest;
-using OrchardCore.Modules.Manifest;
+using OrchardCore.DisplayManagement.Manifest;
 
 [assembly: Theme(
-Name = "EasyOC Site Theme",
-Author = "The EasyOC Team",
-Version = "0.0.1",
+    Name = "EasyOC Site Theme",
+    Author = "The EasyOC Team",
+    Version = "0.0.1",
 // Tags = new[] { "Default" },
-Dependencies = new[]
-{
-    "EasyOC.Core"
-},
-BaseTheme = "TheTheme",
-Description = "EasyOC Site Theme"
+    Dependencies = new[]
+    {
+        "EasyOC.Core"
+    },
+    Description = "EasyOC Site Theme",
+    Tags = new[] { "Bootstrap", "Default" }
 )]
