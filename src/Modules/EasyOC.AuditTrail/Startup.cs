7359f54bc206d2323ae90bc66c20427151c06643
hyzx86 <huarui2219@qq.com>
2022-10-27 22:30:53 +0800

    类型审计功能故障，因为 correlationId 只能保存26 个字符，但是把ContentTypeName 和字段名称都塞进去的话 会造成长度过长，暂时移除此功能，后续打算使用内容项保存变更记录

--- src/Modules/EasyOC.AuditTrail/Startup.cs
+++ src/Modules/EasyOC.AuditTrail/Startup.cs
@@ -1,16 +1,12 @@
 // using EasyOC.AuditTrail.Drivers;
-using EasyOC.AuditTrail.Handlers;
 using EasyOC.AuditTrail.Services;
-using System;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
-using OrchardCore.AuditTrail.Models;
 using OrchardCore.AuditTrail.Services.Models;
-using OrchardCore.ContentTypes.Events;
-using OrchardCore.DisplayManagement.Handlers;
 using OrchardCore.Modules;
+using System;
 
 namespace EasyOC.AuditTrail
 {
@@ -19,7 +15,7 @@ namespace EasyOC.AuditTrail
         public override void ConfigureServices(IServiceCollection services)
         {
             services.AddTransient<IConfigureOptions<AuditTrailOptions>, ContentTypeAuditTrailEventConfiguration>();
-            services.AddScoped<IContentDefinitionEventHandler, AuditTrailContentTypeHandler>();
+            //services.AddScoped<IContentDefinitionEventHandler, AuditTrailContentTypeHandler>();
             // services.AddScoped<IDisplayDriver<AuditTrailEvent>, AuditTrailContentTypeEventDisplayDriver>();
 
         }
